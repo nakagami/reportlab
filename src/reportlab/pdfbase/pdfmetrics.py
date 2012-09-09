9d2a6c84c72b6b1fdd66c35a1811398201b40c1a
Hajime Nakagami <nakagami@gmail.com>
2012-09-09 09:53:53 +0900

    assert fontName is str

--- src/reportlab/pdfbase/pdfmetrics.py
+++ src/reportlab/pdfbase/pdfmetrics.py
@@ -684,6 +684,7 @@ def getEncoding(encName):
 
 def findFontAndRegister(fontName):
     '''search for and register a font given its name'''
+    assert type(fontName) is str
     #it might have a font-specific encoding e.g. Symbol
     # or Dingbats.  If not, take the default.
     face = getTypeFace(fontName)
